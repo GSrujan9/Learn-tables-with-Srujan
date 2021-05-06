d93fee3f7560f7e3a4adfe7be90f42bd5e3db49a
G.Srujan <76508291+GSrujan9@users.noreply.github.com>
2021-05-06 19:14:01 +0530

    Add files via upload

---
+++ Learn tables app coding.js
@@ -0,0 +1,196 @@
+setScreen("home");
+onEvent("previouspage1", "click", function( ) {
+  setScreen("tables1-8");
+});
+onEvent("previouspage2", "click", function( ) {
+  setScreen("tables9-16");
+});
+onEvent("Nextpage", "click", function( ) {
+  setScreen("tables9-16");
+});
+onEvent("Nextpage2", "click", function( ) {
+  setScreen("tables17-20");
+});
+onEvent("1tables", "click", function( ) {
+  setScreen("1Tablespage");
+});
+onEvent("Backtohome1", "click", function( ) {
+  setScreen("tables1-8");
+});
+onEvent("2Tables", "click", function( ) {
+  setScreen("2Tablespage");
+});
+onEvent("Backtohome2", "click", function( ) {
+  setScreen("tables1-8");
+});
+onEvent("3Tables", "click", function( ) {
+  setScreen("3Tablespage");
+});
+onEvent("Backtohome3", "click", function( ) {
+  setScreen("tables1-8");
+});
+onEvent("4Tables", "click", function( ) {
+  setScreen("4Tablespage");
+});
+onEvent("Backtohome4", "click", function( ) {
+  setScreen("tables1-8");
+});
+onEvent("5Tables", "click", function( ) {
+  setScreen("5Tablespage");
+});
+onEvent("Backtohome5", "click", function( ) {
+  setScreen("tables1-8");
+});
+onEvent("6Tables", "click", function( ) {
+  setScreen("6Tablespage");
+});
+onEvent("Backtohome6", "click", function( ) {
+  setScreen("tables1-8");
+});
+onEvent("7Tables", "click", function( ) {
+  setScreen("7Tablespage");
+});
+onEvent("Backtohome7", "click", function( ) {
+  setScreen("tables1-8");
+});
+onEvent("8Tables", "click", function( ) {
+  setScreen("8Tablespage");
+});
+onEvent("Backtohome8", "click", function( ) {
+  setScreen("tables1-8");
+});
+onEvent("9Tables", "click", function( ) {
+  setScreen("9Tablespage");
+});
+onEvent("Backtohome9", "click", function( ) {
+  setScreen("tables9-16");
+});
+onEvent("10Tables", "click", function( ) {
+  setScreen("10Tablespage");
+});
+onEvent("Backtohome10", "click", function( ) {
+  setScreen("tables9-16");
+});
+onEvent("11Tables", "click", function( ) {
+  setScreen("11Tablespage");
+});
+onEvent("Backtohome11", "click", function( ) {
+  setScreen("tables9-16");
+});
+onEvent("12Tables", "click", function( ) {
+  setScreen("12Tablespage");
+});
+onEvent("Backtohome12", "click", function( ) {
+  setScreen("tables9-16");
+});
+onEvent("13Tables", "click", function( ) {
+  setScreen("13Tablespage");
+});
+onEvent("Backtohome13", "click", function( ) {
+  setScreen("tables9-16");
+});
+onEvent("14Tables", "click", function( ) {
+  setScreen("14Tablespage");
+});
+onEvent("Backtohome14", "click", function( ) {
+  setScreen("tables9-16");
+});
+onEvent("15Tables", "click", function( ) {
+  setScreen("15Tablespage");
+});
+onEvent("Backtohome15", "click", function( ) {
+  setScreen("tables9-16");
+});
+onEvent("16Tables", "click", function( ) {
+  setScreen("16Tablespage");
+});
+onEvent("Backtohome16", "click", function( ) {
+  setScreen("tables9-16");
+});
+onEvent("17Tables", "click", function( ) {
+  setScreen("17Tablespage");
+});
+onEvent("Backtohome17", "click", function( ) {
+  setScreen("tables17-20");
+});
+onEvent("18Tables", "click", function( ) {
+  setScreen("18Tablespage");
+});
+onEvent("Backtohome18", "click", function( ) {
+  setScreen("tables17-20");
+});
+onEvent("19Tables", "click", function( ) {
+  setScreen("19Tablespage");
+});
+onEvent("Backtohome19", "click", function( ) {
+  setScreen("tables17-20");
+});
+onEvent("20Tables", "click", function( ) {
+  setScreen("20Tablespage");
+});
+onEvent("Backtohome20", "click", function( ) {
+  setScreen("tables17-20");
+});
+onEvent("Readaloud1", "click", function( ) {
+  playSpeech("one one zar one: one two zar two: one three zar three: one four zar four: one five zar five: one six zar six: one seven zar seven: one eight zar eight: one nine zar nine: one ten  zar ten", "male", "English");
+});
+onEvent("Readaloud2", "click", function( ) {
+  playSpeech("2 one zar two: two two zar four: two three zar six: two four zar eight: two five zar ten: two six zar twelve: two seven zar forteen: two eight zar 16: two nine zar eighteen: two ten  zar twenty", "male", "English");
+});
+onEvent("Readaloud3", "click", function( ) {
+  playSpeech("three one zar three: three two zar six: three three zar nine: three four zar twelve: three five zar fifteen: three six zar eighteen: three seven zar 21: three eight zar 24: three nine zar 27: three ten  zar 30", "male", "English");
+});
+onEvent("Readaloud5", "click", function( ) {
+  playSpeech("5 one zar 5: 5 two zar 10: 5 three zar 15: 5 four zar 20: 5 five zar 25: 5 six zar 30: 5 seven zar 35: 5 eight zar 40: 5 nine zar 45: 5 ten  zar 50", "male", "English");
+});
+onEvent("Readaloud6", "click", function( ) {
+  playSpeech("6 one zar 6: 6 two zar 12: 6 three zar 18: 6 four zar 24: 6 five zar 30: 6 six zar 36: 6 seven zar 42: 6 eight zar 48: 6 nine zar 54: 6 ten zar 60", "male", "English");
+});
+onEvent("Readaloud7", "click", function( ) {
+  playSpeech("7 one zar 7: 7 two zar 14: 7 three zar 21: 7 four zar 28: 7 five zar 35: 7 six zar 42: 7 seven zar 49: 7 eight zar 56: 7 nine zar 63: 7 ten zar 70", "male", "English");
+});
+onEvent("Readaloud8", "click", function( ) {
+  playSpeech("8 one zar 8: 8 two zar 16: 8 three zar 24: 8 four zar 32: 8 five zar 40: 8 six zar 48: 8 seven zar 56: 8 eight zar 64: 8 nine zar 72: 8 ten zar 80", "male", "English");
+});
+onEvent("Readaloud9", "click", function( ) {
+  playSpeech("9 one zar 9: 9 two zar 18: 9 three zar 27: 9 four zar 36: 9 five zar 45: 9 six zar 54: 9 seven zar 63: 9 eight zar 72: 9 nine zar 81: 9 ten zar 90", "male", "English");
+});
+onEvent("Readaloud10", "click", function( ) {
+  playSpeech("10 one zar 10: 10 two zar 20: 10 three zar 30: 10 four zar 40: 10 five zar 50: 10 six zar 60: 10 seven zar 70: 10 eight zar 80: 10 nine zar 90: 10 ten zar 100", "male", "English");
+});
+onEvent("Readaloud11", "click", function( ) {
+  playSpeech("11 one zar 11: 11 two zar 22: 11 three zar 33: 11 four zar 44: 11 five zar 55: 11 six zar 66: 11 seven zar 77: 11 eight zar 88: 11 nine zar 99: 11 ten zar 110", "male", "English");
+});
+onEvent("Readaloud12", "click", function( ) {
+  playSpeech("12 one zar 12: 12 two zar 24: 12 three zar 36: 12 four zar 48: 12 five zar 60: 12 six zar 72: 12 seven zar 84: 12 eight zar 96: 12 nine zar 108: 12 ten zar 120", "male", "English");
+});
+onEvent("Readaloud13", "click", function( ) {
+  playSpeech("13 one zar 13: 13 two zar 26: 13 three zar 39: 13 four zar 52: 13 five zar 65: 13 six zar 78: 13 seven zar 91: 13 eight zar 104: 13 nine zar 117: 13 ten zar 130", "male", "English");
+});
+onEvent("Readaloud14", "click", function( ) {
+  playSpeech("14 one zar 14: 14 two zar 28: 14 three zar 42: 14 four zar 56: 14 five zar 70: 14 six zar 84: 14 seven zar 98: 14 eight zar 112: 14 nine zar 126: 14 ten zar 140", "male", "English");
+});
+onEvent("Readaloud15", "click", function( ) {
+  playSpeech("15 one zar 15: 15 two zar 30: 15 three zar 42: 15 four zar 60: 15 five zar 75: 15 six zar 90: 15 seven zar 105: 15 eight zar 135: 15 nine zar 135: 15 ten zar 150", "male", "English");
+});
+onEvent("Readaloud16", "click", function( ) {
+  playSpeech("16 one zar 16: 16 two zar 32: 16 three zar 48: 16 four zar 64: 16 five zar 80: 16 six zar 96: 16 seven zar 112: 16 eight zar 128: 16 nine zar 114: 16 ten zar 160", "male", "English");
+});
+onEvent("Readaloud17", "click", function( ) {
+  playSpeech("17 one zar 17: 17 two zar 34: 17 three zar 51: 17 four zar 68: 17 five zar 85: 17 six zar 102: 17 seven zar 119: 17 eight zar 136: 17 nine zar 153: 17 ten zar 170", "male", "English");
+});
+onEvent("Readaloud18", "click", function( ) {
+  playSpeech("18 one zar 18: 18 two zar 36: 18 three zar 54: 18 four zar 72: 18 five zar 90: 18 six zar 108: 18 seven zar 126: 18 eight zar 144: 18 nine zar 162: 18 ten zar 180", "male", "English");
+});
+onEvent("Readaloud19", "click", function( ) {
+  playSpeech("19 one zar 19: 19 two zar 38: 19 three zar 57: 19 four zar 76: 19 five zar 95: 19 six zar 114: 19 seven zar 133: 19 eight zar 152: 19 nine zar 171: 19 ten zar 190", "male", "English");
+});
+onEvent("Readaloud20", "click", function( ) {
+  playSpeech("20 one zar 20: 20 two zar 40: 20 three zar 60: 20 four zar 80: 20 five zar 10: 20 six zar 120: 20 seven zar 140: 20 eight zar 160: 20 nine zar 180: 20 ten zar 200", "male", "English");
+});
+onEvent("learntables", "click", function( ) {
+  setScreen("tables1-8");
+});
+onEvent("label6", "click", function( ) {
+  setScreen("tables1-8");
+});
